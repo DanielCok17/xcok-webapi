ad4119494e9f1270ff38a81ce0209a1ccfbf72ef
DanielCok17 <cokydano@gmail.com>
2024-05-26 22:53:08 +0200

    updated before integration

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -29,8 +29,11 @@ switch ($command) {
     "test" {
         go test -v ./...
     }
-        "mongo" {
+    "mongo" {
     mongo up
+    }
+    "docker" {
+        docker build -t danielcok17/xcok-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
